6f1c0f0bc768ab00beda32a0a492884123b7dd4d
prince joseph udoewah <princepiper28@gmail.com>
2025-08-28 00:10:19 +0100

    Fix: adjusted indentation for additional Text components in index.tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -7,8 +7,8 @@ export default function App() {
 
       <View>
         <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-        <Text style={styles.smallText}>ALX is awesome</Text>
+  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+  <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
   );
