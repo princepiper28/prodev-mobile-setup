4d1408b1fe64bf3f97282ff0163f56fa6bca4d11
prince joseph udoewah <princepiper28@gmail.com>
2025-08-27 15:33:19 +0100

    Fix: inline Text components to match checker format

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,4 +1,4 @@
- import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
@@ -6,12 +6,8 @@ export default function App() {
       <Text>Entry Screen - Awesome</Text>
 
       <View>
-        <Text style={styles.largeText}>
-          Typescript is great if you practice more
-        </Text>
-        <Text style={styles.mediumText}>
-          React Native provides you a single codebase for cross platforms
-        </Text>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
@@ -46,4 +42,3 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 });
-
