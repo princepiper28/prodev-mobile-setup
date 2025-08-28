ffb2844644d641e973fc9e45f2dc67c8384bfe05
prince joseph udoewah <princepiper28@gmail.com>
2025-08-28 14:12:42 +0100

    Implement login screen with core components, SafeArea, and styles (prodev-mobile-app-0x03)

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -9,8 +9,9 @@ export default function Index() {
       <SafeAreaView style={styles.container}>
         <View style={styles.navGroup}>
           <Ionicons name="arrow-back" size={25} />
-          <Image source={require('@/assets/images/logo.png')} />
+          <Image source={require("@/assets/images/logo.png")} />
         </View>
+
         <Text style={styles.largeText}>Sign in to your</Text>
         <Text style={styles.largeText}>Account</Text>
         <Text style={styles.smallText}>
@@ -22,6 +23,7 @@ export default function Index() {
             <Text style={styles.placeholderText}>Email</Text>
             <TextInput keyboardType="email-address" style={styles.inputField} />
           </View>
+
           <View style={{ marginTop: 20 }}>
             <Text style={styles.placeholderText}>Password</Text>
             <View style={styles.passwordGroup}>
@@ -29,6 +31,7 @@ export default function Index() {
               <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
             </View>
           </View>
+
           <Text style={styles.forgotPasswordText}>Forgot password?</Text>
         </View>
 
@@ -37,17 +40,9 @@ export default function Index() {
         </TouchableOpacity>
 
         <View style={styles.dividerGroup}>
-          <View
-            style={styles.divider}
-          ></View>
-          <Text
-            style={styles.dividerText}
-          >
-            OR
-          </Text>
-          <View
-            style={styles.divider}
-          ></View>
+          <View style={styles.divider}></View>
+          <Text style={styles.dividerText}>OR</Text>
+          <View style={styles.divider}></View>
         </View>
 
         <View style={styles.socialMediaButtonGroup}>
